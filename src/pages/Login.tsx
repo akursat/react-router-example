28eef157b327d487c634ee4b84ff98fa47def9f8
A. Kürşat Uzun <ademkursatuzun@gmail.com>
2021-11-12 19:00:35 +0300

    upgrade to v6.0.2

---
+++ src/pages/Login.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { toast } from 'react-toastify'
+import { selectIsAuthenticated } from 'features/auth/authSlice'
+import { useSelector } from 'react-redux'
+import { useLoginMutation, useProtectedMutation } from 'features/auth/auth'
+
+function Login() {
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const [login] = useLoginMutation()
+  const [attemptAccess] = useProtectedMutation()
+
+  const loginAsUser = async () => {
+    try {
+      await login({
+        username: 'user',
+        password: 'pass',
+      })
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  const loginAsAdmin = async () => {
+    try {
+      await login({
+        username: 'admin',
+        password: 'pass',
+      })
+    } catch (err) {
+      console.log('err', err)
+    }
+  }
+
+  const makeAuthRequest = () => {
+    attemptAccess()
+      .unwrap()
+      .then((e) => toast(e.message))
+      .catch((e) => toast(e.data.message))
+  }
+
+  return (
+    <div className="content">
+      {isAuthenticated ? (
+        <>
+          <p>You're logged-in.</p>
+        </>
+      ) : (
+        <>
+          <p>You're in the Home page. Login to visit protected pages.</p>
+          <button className="button" onClick={loginAsUser}>
+            Login as User
+          </button>
+          <button className="button" onClick={loginAsAdmin}>
+            Login as Admin
+          </button>
+        </>
+      )}
+      <button className="button" onClick={makeAuthRequest}>
+        Authenticated request
+      </button>
+    </div>
+  )
+}
+
+export default Login
